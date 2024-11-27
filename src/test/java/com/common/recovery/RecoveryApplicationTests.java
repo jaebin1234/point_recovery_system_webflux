dc518c6c8fd9edaec6e0555689d90c6b75fa4547
jaebin1234 <jaebin1234>
2024-11-27 20:13:50 +0900

    init:프로젝트셋팅

---
+++ src/test/java/com/common/recovery/RecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.recovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
